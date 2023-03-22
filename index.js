a6c13e73cf02a9f9ec275a70af5457da972a18e1
Mohamed4900 <Mohamedibnlakhdar10@gmail.com>
2023-03-22 16:17:58 +0100

    photos

--- index.js
+++ index.js
@@ -50,7 +50,7 @@ const wilderList = [
   new Wilder(
     "Julien Benoit",  
     "C'est un métier qui peut être très bien payé.",
-    "resources/promoWilder/Julien.jpg",
+    "resources/Julien.jpg",
     "https://github.com/JulienBenoit7",
     "https://www.linkedin.com/in/julien-benoit-0bb741145/",
   
@@ -58,7 +58,7 @@ const wilderList = [
   new Wilder(
     "Alexandre Oudin",
     "j'ai choisi le métier de développeur car c'est un métier que je peux faire avec mes handicaps et que la technologie ma passionne sous tous les angles",
-    "resources/promoWilder/AlexandreO.jpg",
+    "resources/AlexandreO.jpg",
     "https://github.com/dragowolf51",
     "https://www.linkedin.com/in/alexandre-oudin-b66b98236/",
     
@@ -66,7 +66,7 @@ const wilderList = [
   new Wilder(
     "Flavien Jovic",   
     "C'est un métier qui peut être très bien payé.",
-    "resources/promoWilder/Flavien.jpeg",
+    "resources/Flavien.jpeg",
     "https://github.com/FlavienJovic",
     "https://www.linkedin.com/in/flavien-jovic-4348681b2/",
   ),
@@ -81,7 +81,7 @@ const wilderList = [
   new Wilder(
     "Ahmed El Amri",  
     "Je compte continuer en alternance, pour continuer à apprendre tout en étant encadré par des dev expérimenter.Avoir une première expérience dans le domaine, et décrocher mon titre.",
-    "resources/promoWilder/Ahmed.jpeg",
+    "resources/Ahmed.jpeg",
     "https://github.com/kirikouu78130",
     "https://www.linkedin.com/in/ahmed-el-amri-702083263/",
   
@@ -89,7 +89,7 @@ const wilderList = [
   new Wilder(
     "Alexandre Pompidou",  
     " Je veux faire ce métier par passions.",
-    "resources/promoWilder/AlexandreP.jpeg",
+    "resources/AlexandreP.jpeg",
     "https://github.com/Alex-5110",
     "https://www.linkedin.com/in/alexandre-pompidou-13a467223/",
   
@@ -97,7 +97,7 @@ const wilderList = [
   new Wilder(
     "Aurélien Carenjot",  
     "Je préfère le contact et le sociable au travail en remote même si le cliché du dev montre le contraire",
-    "resources/promoWilder/Aurelien.jpg",
+    "resources/Aurelien.jpg",
     "https://github.com/Carenjot",
     "https://www.linkedin.com/in/aur%C3%A9lien-carenjot-5b55b5162/",
   
@@ -105,7 +105,7 @@ const wilderList = [
   new Wilder(
     "Aylin Noiret",  
     "Après la formation, j’aimerai faire une alternance pour peaufiner mes compétences et me perfectionner dans le domaine.e",
-    "resources/promoWilder/Aylin.JPG",
+    "resources/Aylin.JPG",
     "https://github.com/Nayxl",
     "https://www.linkedin.com/in/aylin-noiret-5370a6269/",
   
@@ -113,7 +113,7 @@ const wilderList = [
   new Wilder(
     "Célia Kleczynski",  
     "J'ai choisi le métier de développeur parce que c'est un domaine qui m'intéressais depuis toujours.",
-    "resources/promoWilder/Celia.jpeg",
+    "resources/Celia.jpeg",
     "https://github.com/CeliaKlc",
     "https://www.linkedin.com/in/c%C3%A9lia-kleczynski-4b556b268/",
   
@@ -121,7 +121,7 @@ const wilderList = [
   new Wilder(
     "Christophe Dubois",  
     "J ai choisie le métier de développeur car j'adore utilisé la logique informatique et humaine pour codée des programmes , on apprends des choses nouvelles tout les jours et la réussite de celle ci est très satisfaisante. ",
-    "resources/promoWilder/Christophe.jpg",
+    "resources/Christophe.jpg",
     "https://github.com/tetsuya51",
     "https://www.linkedin.com/in/christophe-dubois-217452186/",
   
@@ -129,7 +129,7 @@ const wilderList = [
   new Wilder(
     "Elodie Jehl",  
     "C'est un métier d'avenir, qui permet d'intervenir dans beaucoup de domaines différents. Il permet également de créer des choses à partir de rien, d'améliorer de l'existant et d'aider des populations.",
-    "resources/promoWilder/Elodie.png",
+    "resources/Elodie.png",
     "https://github.com/Elodie0530",
     "",
   
@@ -137,7 +137,7 @@ const wilderList = [
   new Wilder(
     "Enzo Andre",  
     "Mon projet est de demarrer une alternance de 1 an puis trouver un travail pour me lancer dans la vie active",
-    "resources/promoWilder/Enzo.jpg",
+    "resources/Enzo.jpg",
     "https://github.com/Enzo-adr5",
     "https://www.linkedin.com/in/enzo-andre-7ba6a7269/",
   
@@ -145,7 +145,7 @@ const wilderList = [
   new Wilder(
     "Maximilien Thiry",  
     "(Pour l'argent Pardi )  C'est un métier que je trouve intéressant ou tu dois te poser des questions sur comment tu peux faire et aussi le pouvoir de Créer.. Aussi pour voyager.",
-    "resources/promoWilder/Maximilien.jpeg",
+    "resources/Maximilien.jpeg",
     "https://github.com/zeitounmax",
     "https://www.linkedin.com/in/maximilienthiry/",
   
@@ -153,7 +153,7 @@ const wilderList = [
   new Wilder(
     "Florian Geron",  
     "J'ai choisi le métier de développeur car j'ai déjà été en étude de production graphique et je souhaite ajouter des compétences à mon profil.",
-    "resources/promoWilder/Florian.jpeg",
+    "resources/Florian.jpeg",
     "https://github.com/Kika051",
     "https://www.linkedin.com/in/florian-geron-613042224/",
   
@@ -161,7 +161,7 @@ const wilderList = [
   new Wilder(
     "Mohamed Berkoussi",  
     "J'aime le métier de dév. web car il me permet de créer des projets intéressants et interactifs en utilisant mes compétences en codage, de travailler en équipe avec des autres dév. et de bénéficier de nombreuses opportunités de carrière.",
-    "resources/promoWilder/MohamedB.jpg",
+    "resources/MohamedB.jpg",
     "https://github.com/Mohamed4900",
     "https://www.linkedin.com/in/mohamed-berkoussi4900/",
   
@@ -169,7 +169,7 @@ const wilderList = [
   new Wilder(
     "Mohamed Maach",  
     "Ma reconversion professionnelle du génie énergétique au développement web est motivée par la passion pour la technologie et la création de produits numériques.",
-    "resources/promoWilder/MohamedM.jpeg",
+    "resources/MohamedM.jpeg",
     "https://github.com/Maachino",
     "https://www.linkedin.com/in/mmaach/",
   
